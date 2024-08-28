b1330c8164ae5e9185e35e46abf9fb472d910d25
moichi <zzikbu@gmail.com>
2024-08-28 17:44:14 +0900

    chore: 색상 설정

---
+++ assets/pallete.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  Pallete._();
+
+  static const forestGreen = Color(0xFF419461);
+  static const darkGray = Color(0xFF343834);
+  static const mediumGray = Color(0xFF777777);
+  static const lightGray = Color(0xFFD9D9D9);
+}
