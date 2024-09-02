0b802d9d205a3fa636103ccb1f5e5b0c7ba24db3
moichi <zzikbu@gmail.com>
2024-09-02 16:35:42 +0900

    feat: 성장일기 홈 페이지 UI 구현

---
+++ lib/diary/diary_home_page.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:pet_log/pallete.dart';
+
+class DiaryHomePage extends StatelessWidget {
+  const DiaryHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Pallete.background,
+      appBar: AppBar(
+        backgroundColor: Pallete.background,
+        scrolledUnderElevation: 0,
+        centerTitle: true,
+        title: Text(
+          "성장일기",
+          style: TextStyle(
+            fontFamily: 'Pretendard',
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+            color: Pallete.black,
+            letterSpacing: -0.5,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: GestureDetector(
+              onTap: () {},
+              child: SvgPicture.asset('assets/icons/ic_magnifier.svg'),
+            ),
+          ),
+        ],
+      ),
+      body: Scrollbar(
+        child: ListView.builder(
+          padding: const EdgeInsets.fromLTRB(24, 0, 24, 24),
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => WalkDetailPage()),
+                // );
+              },
+              child: Stack(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    height: 220,
+                    decoration: BoxDecoration(
+                      color: Pallete.mainGreen,
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        color: Pallete.feedBoder,
+                        width: 1,
+                        style: BorderStyle.solid,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Pallete.black.withOpacity(0.05),
+                          offset: Offset(8, 8),
+                          blurRadius: 8,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    bottom: 20,
+                    child: Container(
+                      height: 88,
+                      decoration: BoxDecoration(
+                        color: Pallete.white.withOpacity(0.94),
+                        border: Border(
+                          left: BorderSide(
+                            color: Pallete.feedBoder,
+                            width: 1,
+                            style: BorderStyle.solid,
+                          ),
+                          right: BorderSide(
+                            color: Pallete.feedBoder,
+                            width: 1,
+                            style: BorderStyle.solid,
+                          ),
+                          bottom: BorderSide(
+                            color: Pallete.feedBoder,
+                            width: 1,
+                            style: BorderStyle.solid,
+                          ),
+                        ),
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 22, left: 14),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                SvgPicture.asset(
+                                  'assets/icons/ic_unlock_small.svg',
+                                ),
+                                SizedBox(width: 4),
+                                Text(
+                                  '2024.08.14',
+                                  style: TextStyle(
+                                    fontFamily: 'Pretendard',
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 14,
+                                    color: Pallete.mediumGray,
+                                    letterSpacing: -0.35,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 2),
+                              child: Text(
+                                '다같이 애견카페 가서 놀은 날',
+                                style: TextStyle(
+                                  fontFamily: 'Pretendard',
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18,
+                                  color: Pallete.black,
+                                  letterSpacing: -0.4,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+          itemCount: 9,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigator.push(
+          //   context,
+          //   MaterialPageRoute(builder: (context) => WalkSelectPetPage()),
+          // );
+        },
+        backgroundColor: Pallete.darkGray,
+        elevation: 0, // 그림자 제거
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Icon(
+          Icons.edit,
+          color: Pallete.white,
+        ),
+      ),
+    );
+  }
+}
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pet_log/pallete.dart';
 import 'package:pet_log/walk/walk_home_page.dart';
 
+import '../diary/diary_home_page.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -16,45 +18,94 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Pallete.background,
-      body: Center(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "산책",
-              style: TextStyle(
-                fontFamily: 'Pretendard',
-                fontWeight: FontWeight.w600,
-                fontSize: 20,
-                color: Pallete.black,
-                letterSpacing: -0.5,
-              ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  "산책",
+                  style: TextStyle(
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20,
+                    color: Pallete.black,
+                    letterSpacing: -0.5,
+                  ),
+                ),
+                Spacer(),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => WalkHomePage()),
+                    );
+                  },
+                  child: Row(
+                    children: [
+                      Text(
+                        "더보기",
+                        style: TextStyle(
+                          fontFamily: 'Pretendard',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12,
+                          color: Pallete.mediumGray,
+                          letterSpacing: -0.3,
+                        ),
+                      ),
+                      SvgPicture.asset(
+                        'assets/icons/ic_home_more.svg',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Spacer(),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => WalkHomePage()),
-                );
-              },
-              child: Row(
-                children: [
-                  Text(
-                    "더보기",
-                    style: TextStyle(
-                      fontFamily: 'Pretendard',
-                      fontWeight: FontWeight.w400,
-                      fontSize: 12,
-                      color: Pallete.mediumGray,
-                      letterSpacing: -0.3,
-                    ),
+            SizedBox(height: 40),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  "성장일기",
+                  style: TextStyle(
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20,
+                    color: Pallete.black,
+                    letterSpacing: -0.5,
                   ),
-                  SvgPicture.asset(
-                    'assets/icons/ic_home_more.svg',
+                ),
+                Spacer(),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DiaryHomePage()),
+                    );
+                  },
+                  child: Row(
+                    children: [
+                      Text(
+                        "더보기",
+                        style: TextStyle(
+                          fontFamily: 'Pretendard',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12,
+                          color: Pallete.mediumGray,
+                          letterSpacing: -0.3,
+                        ),
+                      ),
+                      SvgPicture.asset(
+                        'assets/icons/ic_home_more.svg',
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
