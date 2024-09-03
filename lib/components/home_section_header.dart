3d278638baf77fc82b92edcc132423f7e34e62fa
moichi <zzikbu@gmail.com>
2024-09-03 15:43:53 +0900

    feat: 최상단 CarouselSlider 구현

---
+++ lib/components/home_section_header.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../pallete.dart';
+
+class HomeSectionHeader extends StatelessWidget {
+  final String title;
+  final Function() onTap;
+
+  const HomeSectionHeader({
+    super.key,
+    required this.title,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            fontFamily: 'Pretendard',
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+            color: Pallete.black,
+            letterSpacing: -0.5,
+          ),
+        ),
+        Spacer(),
+        GestureDetector(
+          onTap: onTap,
+          child: Row(
+            children: [
+              Text(
+                "더보기",
+                style: TextStyle(
+                  fontFamily: 'Pretendard',
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12,
+                  color: Pallete.mediumGray,
+                  letterSpacing: -0.3,
+                ),
+              ),
+              SvgPicture.asset(
+                'assets/icons/ic_home_more.svg',
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:carousel_slider/carousel_controller.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pet_log/components/home_section_header.dart';
 import 'package:pet_log/medical/medical_home_page.dart';
 import 'package:pet_log/pallete.dart';
 import 'package:pet_log/walk/walk_home_page.dart';
@@ -16,142 +18,102 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int currentPageIndex = 1;
+  int currentpetPageIndex = 0;
+
+  final CarouselSliderController _controller = CarouselSliderController();
+
+  List colorList = [
+    Colors.red,
+    Colors.blue,
+    Colors.grey,
+    Colors.yellow,
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Pallete.background,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  "산책",
-                  style: TextStyle(
-                    fontFamily: 'Pretendard',
-                    fontWeight: FontWeight.w600,
-                    fontSize: 20,
-                    color: Pallete.black,
-                    letterSpacing: -0.5,
+            Container(
+              height: 260,
+              color: Pallete.mainGreen,
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 24, bottom: 42),
+                  child: CarouselSlider(
+                    carouselController: _controller,
+                    items: colorList.map(
+                      (color) {
+                        return Builder(
+                          builder: (context) {
+                            return Container(
+                              // width: MediaQuery.of(context).size.width - 48,
+                              margin: EdgeInsets.symmetric(horizontal: 24),
+                              decoration: BoxDecoration(
+                                color: color,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ).toList(),
+                    options: CarouselOptions(
+                      initialPage: 0,
+                      height: 150,
+                      viewportFraction: 1.0,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          currentpetPageIndex = index;
+                          print(index);
+                        });
+                      },
+                    ),
                   ),
                 ),
-                Spacer(),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => WalkHomePage()),
-                    );
-                  },
-                  child: Row(
-                    children: [
-                      Text(
-                        "더보기",
-                        style: TextStyle(
-                          fontFamily: 'Pretendard',
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
-                          color: Pallete.mediumGray,
-                          letterSpacing: -0.3,
-                        ),
-                      ),
-                      SvgPicture.asset(
-                        'assets/icons/ic_home_more.svg',
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+              ),
             ),
             SizedBox(height: 40),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  "성장일기",
-                  style: TextStyle(
-                    fontFamily: 'Pretendard',
-                    fontWeight: FontWeight.w600,
-                    fontSize: 20,
-                    color: Pallete.black,
-                    letterSpacing: -0.5,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  HomeSectionHeader(
+                    title: '산책',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => WalkHomePage()),
+                      );
+                    },
                   ),
-                ),
-                Spacer(),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => DiaryHomePage()),
-                    );
-                  },
-                  child: Row(
-                    children: [
-                      Text(
-                        "더보기",
-                        style: TextStyle(
-                          fontFamily: 'Pretendard',
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
-                          color: Pallete.mediumGray,
-                          letterSpacing: -0.3,
-                        ),
-                      ),
-                      SvgPicture.asset(
-                        'assets/icons/ic_home_more.svg',
-                      ),
-                    ],
+                  SizedBox(height: 40),
+                  HomeSectionHeader(
+                    title: '성장일기',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => DiaryHomePage()),
+                      );
+                    },
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 40),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  "진료기록",
-                  style: TextStyle(
-                    fontFamily: 'Pretendard',
-                    fontWeight: FontWeight.w600,
-                    fontSize: 20,
-                    color: Pallete.black,
-                    letterSpacing: -0.5,
+                  SizedBox(height: 40),
+                  HomeSectionHeader(
+                    title: '진료기록',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => MedicalHomePage()),
+                      );
+                    },
                   ),
-                ),
-                Spacer(),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => MedicalHomePage()),
-                    );
-                  },
-                  child: Row(
-                    children: [
-                      Text(
-                        "더보기",
-                        style: TextStyle(
-                          fontFamily: 'Pretendard',
-                          fontWeight: FontWeight.w400,
-                          fontSize: 12,
-                          color: Pallete.mediumGray,
-                          letterSpacing: -0.3,
-                        ),
-                      ),
-                      SvgPicture.asset(
-                        'assets/icons/ic_home_more.svg',
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
@@ -159,149 +121,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
-/*
-import 'package:flutter/material.dart';
-
-/// Flutter code sample for [NavigationBar].
-
-void main() => runApp(const NavigationBarApp());
-
-class NavigationBarApp extends StatelessWidget {
-  const NavigationBarApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: const NavigationExample(),
-    );
-  }
-}
-
-class NavigationExample extends StatefulWidget {
-  const NavigationExample({super.key});
-
-  @override
-  State<NavigationExample> createState() => _NavigationExampleState();
-}
-
-class _NavigationExampleState extends State<NavigationExample> {
-  int currentPageIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        indicatorColor: Colors.amber,
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Badge(child: Icon(Icons.notifications_sharp)),
-            label: 'Notifications',
-          ),
-          NavigationDestination(
-            icon: Badge(
-              label: Text('2'),
-              child: Icon(Icons.messenger_sharp),
-            ),
-            label: 'Messages',
-          ),
-        ],
-      ),
-      body: <Widget>[
-        /// Home page
-        Card(
-          shadowColor: Colors.transparent,
-          margin: const EdgeInsets.all(8.0),
-          child: SizedBox.expand(
-            child: Center(
-              child: Text(
-                'Home page',
-                style: theme.textTheme.titleLarge,
-              ),
-            ),
-          ),
-        ),
-
-        /// Notifications page
-        const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.notifications_sharp),
-                  title: Text('Notification 1'),
-                  subtitle: Text('This is a notification'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.notifications_sharp),
-                  title: Text('Notification 2'),
-                  subtitle: Text('This is a notification'),
-                ),
-              ),
-            ],
-          ),
-        ),
-
-        /// Messages page
-        ListView.builder(
-          reverse: true,
-          itemCount: 2,
-          itemBuilder: (BuildContext context, int index) {
-            if (index == 0) {
-              return Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  margin: const EdgeInsets.all(8.0),
-                  padding: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    color: theme.colorScheme.primary,
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: Text(
-                    'Hello',
-                    style: theme.textTheme.bodyLarge!
-                        .copyWith(color: theme.colorScheme.onPrimary),
-                  ),
-                ),
-              );
-            }
-            return Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                margin: const EdgeInsets.all(8.0),
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: theme.colorScheme.primary,
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Text(
-                  'Hi!',
-                  style: theme.textTheme.bodyLarge!
-                      .copyWith(color: theme.colorScheme.onPrimary),
-                ),
-              ),
-            );
-          },
-        ),
-      ][currentPageIndex],
-    );
-  }
-}
-
- */
