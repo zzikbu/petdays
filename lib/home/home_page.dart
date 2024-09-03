13503056762ae6a58a2d040bd7e857ccdabae967
moichi <zzikbu@gmail.com>
2024-09-03 16:19:47 +0900

    feat: 최상단 CarouselSlider 반려동물 정보 UI 구현

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,6 +1,4 @@
-import 'package:carousel_slider/carousel_controller.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pet_log/components/home_section_header.dart';
 import 'package:pet_log/medical/medical_home_page.dart';
@@ -17,13 +15,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int currentPageIndex = 1;
-  int currentpetPageIndex = 0;
+  int currentPetPageIndex = 0;
 
-  final CarouselSliderController _controller = CarouselSliderController();
+  final CarouselSliderController _myPetCarouselSliderController =
+      CarouselSliderController();
 
   List colorList = [
-    Colors.red,
+    Colors.white,
     Colors.blue,
     Colors.grey,
     Colors.yellow,
@@ -44,17 +42,79 @@ class _HomePageState extends State<HomePage> {
                 child: Padding(
                   padding: const EdgeInsets.only(top: 24, bottom: 42),
                   child: CarouselSlider(
-                    carouselController: _controller,
+                    carouselController: _myPetCarouselSliderController,
                     items: colorList.map(
                       (color) {
                         return Builder(
                           builder: (context) {
                             return Container(
-                              // width: MediaQuery.of(context).size.width - 48,
                               margin: EdgeInsets.symmetric(horizontal: 24),
                               decoration: BoxDecoration(
                                 color: color,
                                 borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 20),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 100,
+                                      height: 100,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        border: Border.all(
+                                          color: Pallete.lightGray,
+                                          width: 0.4,
+                                        ),
+                                        image: DecorationImage(
+                                          image: AssetImage(
+                                              'assets/icons/dummy_dog.jpg'),
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(width: 30),
+                                    Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          'D+2100',
+                                          style: TextStyle(
+                                            fontFamily: 'Pretendard',
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 22,
+                                            color: Pallete.black,
+                                            letterSpacing: -0.5,
+                                          ),
+                                        ),
+                                        Text(
+                                          '보리',
+                                          style: TextStyle(
+                                            fontFamily: 'Pretendard',
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 18,
+                                            color: Pallete.black,
+                                            letterSpacing: -0.5,
+                                          ),
+                                        ),
+                                        Text(
+                                          '5살 치와와',
+                                          style: TextStyle(
+                                            fontFamily: 'Pretendard',
+                                            fontWeight: FontWeight.w600,
+                                            fontSize: 14,
+                                            color: Pallete.mediumGray,
+                                            letterSpacing: -0.4,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
                             );
                           },
@@ -67,7 +127,7 @@ class _HomePageState extends State<HomePage> {
                       viewportFraction: 1.0,
                       onPageChanged: (index, reason) {
                         setState(() {
-                          currentpetPageIndex = index;
+                          currentPetPageIndex = index;
                           print(index);
                         });
                       },
