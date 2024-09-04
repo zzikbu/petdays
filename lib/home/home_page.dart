32c21eaa401607fc2a487903fbeb7cb464f3f94c
moichi <zzikbu@gmail.com>
2024-09-04 16:14:37 +0900

    feat: 홈화면 성장일기 UI 구현

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:pet_log/components/home_section_header.dart';
+import 'package:pet_log/diary/diary_detail_page.dart';
 import 'package:pet_log/medical/medical_home_page.dart';
 import 'package:pet_log/pallete.dart';
 import 'package:pet_log/walk/walk_home_page.dart';
@@ -8,6 +9,7 @@ import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../diary/diary_home_page.dart';
 import '../dummy.dart';
+import '../walk/walk_detail_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -196,74 +198,185 @@ class _HomePageState extends State<HomePage> {
                                 ? 3
                                 : dummyPets.length, // 최대 3개의 아이템 표시
                             (index) {
-                              return Container(
-                                margin: EdgeInsets.only(bottom: 12),
-                                height: 70,
-                                decoration: BoxDecoration(
-                                  color: Pallete.white,
-                                  borderRadius: BorderRadius.circular(20),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Pallete.black.withOpacity(0.05),
-                                      offset: Offset(8, 8),
-                                      blurRadius: 8,
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => WalkDetailPage(),
                                     ),
-                                  ],
-                                ),
-                                child: Row(
-                                  children: [
-                                    SizedBox(width: 14),
-                                    Text(
-                                      "2024.08.16 금",
-                                      style: TextStyle(
-                                        fontFamily: 'Pretendard',
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 16,
-                                        color: Pallete.black,
-                                        letterSpacing: -0.5,
+                                  );
+                                },
+                                child: Container(
+                                  margin: EdgeInsets.only(bottom: 12),
+                                  height: 70,
+                                  decoration: BoxDecoration(
+                                    color: Pallete.white,
+                                    borderRadius: BorderRadius.circular(20),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Pallete.black.withOpacity(0.05),
+                                        offset: Offset(8, 8),
+                                        blurRadius: 8,
                                       ),
-                                    ),
-                                    SizedBox(width: 10),
-                                    Expanded(
-                                      child: ListView.builder(
-                                        padding: EdgeInsets.only(right: 14),
-                                        scrollDirection:
-                                            Axis.horizontal, // 수평 스크롤
-                                        itemCount: dummyPets.length,
-                                        itemBuilder: (context, index) {
-                                          return Container(
-                                            width: 36,
-                                            height: 36,
-                                            margin: EdgeInsets.only(right: 4),
-                                            decoration: BoxDecoration(
-                                              shape: BoxShape.circle,
-                                              image: DecorationImage(
-                                                image: AssetImage(
-                                                  dummyPets[index]['image']!,
+                                    ],
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      SizedBox(width: 14),
+                                      Text(
+                                        "2024.08.16 금",
+                                        style: TextStyle(
+                                          fontFamily: 'Pretendard',
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 16,
+                                          color: Pallete.black,
+                                          letterSpacing: -0.5,
+                                        ),
+                                      ),
+                                      SizedBox(width: 10),
+                                      Expanded(
+                                        child: ListView.builder(
+                                          padding: EdgeInsets.only(right: 14),
+                                          scrollDirection:
+                                              Axis.horizontal, // 수평 스크롤
+                                          itemCount: dummyPets.length,
+                                          itemBuilder: (context, index) {
+                                            return Container(
+                                              width: 36,
+                                              height: 36,
+                                              margin: EdgeInsets.only(right: 4),
+                                              decoration: BoxDecoration(
+                                                shape: BoxShape.circle,
+                                                image: DecorationImage(
+                                                  image: AssetImage(
+                                                    dummyPets[index]['image']!,
+                                                  ),
+                                                  fit: BoxFit.cover,
                                                 ),
-                                                fit: BoxFit.cover,
                                               ),
-                                            ),
-                                          );
-                                        },
+                                            );
+                                          },
+                                        ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
                               );
                             },
                           ),
                         ),
                   SizedBox(height: 28),
-                  HomeSectionHeader(
-                    title: '성장일기',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => DiaryHomePage()),
-                      );
-                    },
+                  Column(
+                    children: [
+                      HomeSectionHeader(
+                        title: '성장일기',
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => DiaryHomePage()),
+                          );
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      if (dummyPets.isEmpty)
+                        Container(
+                          margin: EdgeInsets.only(bottom: 12),
+                          height: 70,
+                          decoration: BoxDecoration(
+                            color: Pallete.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Pallete.black.withOpacity(0.05),
+                                offset: Offset(8, 8),
+                                blurRadius: 8,
+                              ),
+                            ],
+                          ),
+                          child: Center(
+                            child: Text(
+                              "성장일기가 없습니다",
+                              style: TextStyle(
+                                fontFamily: 'Pretendard',
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12,
+                                color: Pallete.mediumGray,
+                                letterSpacing: -0.4,
+                              ),
+                            ),
+                          ),
+                        )
+                      else
+                        Column(
+                          children: List.generate(
+                            dummyPets.length > 3
+                                ? 3
+                                : dummyPets.length, // 최대 3개의 아이템 표시
+                            (index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => DiaryDetailPage(),
+                                    ),
+                                  );
+                                },
+                                child: Container(
+                                  margin: EdgeInsets.only(bottom: 12),
+                                  height: 70,
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    color: Pallete.white,
+                                    borderRadius: BorderRadius.circular(20),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Pallete.black.withOpacity(0.05),
+                                        offset: Offset(8, 8),
+                                        blurRadius: 8,
+                                      ),
+                                    ],
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 14),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "다같이 애견카페 가서 놀은 날",
+                                          style: TextStyle(
+                                            fontFamily: 'Pretendard',
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 14,
+                                            color: Pallete.black,
+                                            letterSpacing: -0.4,
+                                          ),
+                                        ),
+                                        Text(
+                                          "2024.08.14",
+                                          style: TextStyle(
+                                            fontFamily: 'Pretendard',
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 14,
+                                            color: Pallete.mediumGray,
+                                            letterSpacing: -0.4,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                    ],
                   ),
                   SizedBox(height: 40),
                   HomeSectionHeader(
