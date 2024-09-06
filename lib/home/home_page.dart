4b3065376cff5a9fdacc5b558d6875e9ec86615e
moichi <zzikbu@gmail.com>
2024-09-06 13:29:55 +0900

    feat: 홈화면 진료기록 UI 구현

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:pet_log/components/home_section_header.dart';
 import 'package:pet_log/diary/diary_detail_page.dart';
+import 'package:pet_log/medical/medical_detail_page.dart';
 import 'package:pet_log/medical/medical_home_page.dart';
 import 'package:pet_log/pallete.dart';
 import 'package:pet_log/walk/walk_home_page.dart';
@@ -196,7 +198,7 @@ class _HomePageState extends State<HomePage> {
                           children: List.generate(
                             dummyPets.length > 3
                                 ? 3
-                                : dummyPets.length, // 최대 3개의 아이템 표시
+                                : dummyPets.length, // 최대 3개
                             (index) {
                               return GestureDetector(
                                 onTap: () {
@@ -229,7 +231,7 @@ class _HomePageState extends State<HomePage> {
                                         style: TextStyle(
                                           fontFamily: 'Pretendard',
                                           fontWeight: FontWeight.w500,
-                                          fontSize: 16,
+                                          fontSize: 15,
                                           color: Pallete.black,
                                           letterSpacing: -0.5,
                                         ),
@@ -248,6 +250,10 @@ class _HomePageState extends State<HomePage> {
                                               margin: EdgeInsets.only(right: 4),
                                               decoration: BoxDecoration(
                                                 shape: BoxShape.circle,
+                                                border: Border.all(
+                                                  color: Pallete.lightGray,
+                                                  width: 0.4,
+                                                ),
                                                 image: DecorationImage(
                                                   image: AssetImage(
                                                     dummyPets[index]['image']!,
@@ -313,7 +319,7 @@ class _HomePageState extends State<HomePage> {
                           children: List.generate(
                             dummyPets.length > 3
                                 ? 3
-                                : dummyPets.length, // 최대 3개의 아이템 표시
+                                : dummyPets.length, // 최대 3개
                             (index) {
                               return GestureDetector(
                                 onTap: () {
@@ -363,7 +369,7 @@ class _HomePageState extends State<HomePage> {
                                           style: TextStyle(
                                             fontFamily: 'Pretendard',
                                             fontWeight: FontWeight.w400,
-                                            fontSize: 14,
+                                            fontSize: 12,
                                             color: Pallete.mediumGray,
                                             letterSpacing: -0.4,
                                           ),
@@ -378,18 +384,159 @@ class _HomePageState extends State<HomePage> {
                         ),
                     ],
                   ),
-                  SizedBox(height: 40),
-                  HomeSectionHeader(
-                    title: '진료기록',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => MedicalHomePage()),
-                      );
-                    },
+                  SizedBox(height: 28),
+                  Column(
+                    children: [
+                      HomeSectionHeader(
+                        title: '진료기록',
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MedicalHomePage()),
+                          );
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      if (dummyPets.isEmpty)
+                        Container(
+                          margin: EdgeInsets.only(bottom: 12),
+                          height: 70,
+                          decoration: BoxDecoration(
+                            color: Pallete.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Pallete.black.withOpacity(0.05),
+                                offset: Offset(8, 8),
+                                blurRadius: 8,
+                              ),
+                            ],
+                          ),
+                          child: Center(
+                            child: Text(
+                              "진료기록이 없습니다",
+                              style: TextStyle(
+                                fontFamily: 'Pretendard',
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12,
+                                color: Pallete.mediumGray,
+                                letterSpacing: -0.4,
+                              ),
+                            ),
+                          ),
+                        )
+                      else
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal, // 수평 스크롤 설정
+                          child: Row(
+                            children: List.generate(
+                              dummyPets.length > 3
+                                  ? 7
+                                  : dummyPets.length, // 최대 7개
+                              (index) {
+                                return GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            MedicalDetailPage(),
+                                      ),
+                                    );
+                                  },
+                                  child: Container(
+                                    margin:
+                                        EdgeInsets.only(right: 12, bottom: 10),
+                                    height: 170,
+                                    width: 150,
+                                    decoration: BoxDecoration(
+                                      color: Pallete.white,
+                                      borderRadius: BorderRadius.circular(20),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color:
+                                              Pallete.black.withOpacity(0.05),
+                                          offset: Offset(8, 8),
+                                          blurRadius: 8,
+                                        ),
+                                      ],
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 12, horizontal: 20),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              Container(
+                                                width: 36,
+                                                height: 36,
+                                                margin:
+                                                    EdgeInsets.only(right: 4),
+                                                decoration: BoxDecoration(
+                                                  shape: BoxShape.circle,
+                                                  border: Border.all(
+                                                    color: Pallete.lightGray,
+                                                    width: 0.4,
+                                                  ),
+                                                  image: DecorationImage(
+                                                    image: AssetImage(
+                                                      dummyPets[index]
+                                                          ['image']!,
+                                                    ),
+                                                    fit: BoxFit.cover,
+                                                  ),
+                                                ),
+                                              ),
+                                              Text(
+                                                dummyPets[index]['name']!,
+                                                style: TextStyle(
+                                                  fontFamily: 'Pretendard',
+                                                  fontWeight: FontWeight.w500,
+                                                  fontSize: 20,
+                                                  color: Pallete.black,
+                                                  letterSpacing: -0.5,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          SizedBox(height: 18),
+                                          Text(
+                                            "이틀 째 숨을 거칠게 쉬어서",
+                                            style: TextStyle(
+                                              fontFamily: 'Pretendard',
+                                              fontWeight: FontWeight.w400,
+                                              fontSize: 16,
+                                              color: Pallete.black,
+                                              letterSpacing: -0.5,
+                                            ),
+                                          ),
+                                          SizedBox(height: 18),
+                                          Text(
+                                            "2024.08.13",
+                                            style: TextStyle(
+                                              fontFamily: 'Pretendard',
+                                              fontWeight: FontWeight.w400,
+                                              fontSize: 14,
+                                              color: Pallete.mediumGray,
+                                              letterSpacing: -0.4,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                    ],
                   ),
-                  SizedBox(height: 60),
+                  SizedBox(height: 50),
                 ],
               ),
             ),
