9267801c136baf060b4b9a50e18a5483fc537a10
moichi <zzikbu@gmail.com>
2024-09-10 13:22:29 +0900

    design: 반려동물 상세정보 입력 페이지 성별, 중성화 ChoiceChip으로 디자인 수정

--- lib/sign_up/sign_up_pet_info_page.dart
+++ lib/sign_up/sign_up_pet_info_page.dart
@@ -13,8 +13,18 @@ class SignUpPetInfoPage extends StatefulWidget {
 }
 
 class _SignUpPetInfoPageState extends State<SignUpPetInfoPage> {
-  String? _selectedGender;
-  String? _selectedNeutering;
+  String? selectedGender;
+  String? selectedNeutering;
+
+  final List<String> genderTypes = [
+    '수컷',
+    '암컷',
+  ];
+
+  final List<String> NeuteringTypes = [
+    '했어요',
+    '안했어요',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -45,60 +55,6 @@ class _SignUpPetInfoPageState extends State<SignUpPetInfoPage> {
               unselectedColor: Pallete.lightGray,
             ),
           ),
-          SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Text(
-              '반려동물의 상세정보를\n입력해주세요',
-              style: TextStyle(
-                fontFamily: 'Pretendard',
-                fontWeight: FontWeight.w600,
-                fontSize: 24,
-                color: Pallete.black,
-                letterSpacing: -0.6,
-              ),
-            ),
-          ),
-          SizedBox(height: 30),
-          Center(
-            child: Stack(
-              clipBehavior: Clip.none,
-              children: [
-                Container(
-                  width: 120.0,
-                  height: 120.0,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: Pallete.lightGray,
-                      width: 2.0, // 원형 테두리의 두께
-                    ),
-                    color: Colors.white, // 원형의 배경색
-                  ),
-                  child: Icon(
-                    Icons.pets,
-                    color: Colors.black,
-                    size: 40.0,
-                  ),
-                ),
-                Positioned(
-                  bottom: -10,
-                  right: -5,
-                  child: IconButton(
-                    icon: Icon(
-                      Icons.camera_alt,
-                      color: Pallete.black,
-                      size: 30.0,
-                    ),
-                    onPressed: () {
-                      print('카메라 아이콘 눌림');
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 15),
           Expanded(
             child: Scrollbar(
               child: SingleChildScrollView(
@@ -107,57 +63,187 @@ class _SignUpPetInfoPageState extends State<SignUpPetInfoPage> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      SizedBox(height: 15),
-                      TextFieldWithTitle(
-                        labelText: '이름',
-                        maxLength: 5,
-                        hintText: '이름을 입력해주세요',
-                      ),
-                      SizedBox(height: 40),
-                      TextFieldWithTitle(
-                        labelText: '품종',
-                        maxLength: 8,
-                        hintText: '품종을 입력해주세요',
-                      ),
-                      SizedBox(height: 40),
-                      TextFieldWithTitle(
-                        labelText: '생년월일',
-                        hintText: '2000-08-07 형식으로 입력해주세요',
-                        keyboardType: TextInputType.datetime,
-                      ),
-                      SizedBox(height: 40),
-                      TextFieldWithTitle(
-                        labelText: '만날 날',
-                        hintText: '2000-08-07 형식으로 입력해주세요',
-                        keyboardType: TextInputType.datetime,
-                      ),
-                      SizedBox(height: 40),
-                      TextFieldWithTitle(
-                        labelText: '성별',
-                        hintText: '선택해주세요',
-                        isCupertinoPicker: true,
-                        pickerItems: ['수컷', '암컷'],
-                        selectedValue: _selectedGender,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            _selectedGender = newValue;
-                          });
-                        },
-                      ),
-                      SizedBox(height: 40),
-                      TextFieldWithTitle(
-                        labelText: '중성화',
-                        hintText: '선택해주세요',
-                        isCupertinoPicker: true,
-                        pickerItems: ['중성화', '비중성화'],
-                        selectedValue: _selectedNeutering,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            _selectedNeutering = newValue;
-                          });
-                        },
+                      SizedBox(height: 30),
+                      Text(
+                        '반려동물의 상세정보를\n입력해주세요',
+                        style: TextStyle(
+                          fontFamily: 'Pretendard',
+                          fontWeight: FontWeight.w600,
+                          fontSize: 24,
+                          color: Pallete.black,
+                          letterSpacing: -0.6,
+                        ),
                       ),
                       SizedBox(height: 30),
+                      Center(
+                        child: Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            Container(
+                              width: 120.0,
+                              height: 120.0,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: Pallete.lightGray,
+                                  width: 2.0, // 원형 테두리의 두께
+                                ),
+                                color: Colors.white, // 원형의 배경색
+                              ),
+                              child: Icon(
+                                Icons.pets,
+                                color: Colors.black,
+                                size: 40.0,
+                              ),
+                            ),
+                            Positioned(
+                              bottom: -10,
+                              right: -5,
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.camera_alt,
+                                  color: Pallete.black,
+                                  size: 30.0,
+                                ),
+                                onPressed: () {
+                                  print('카메라 아이콘 눌림');
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 15),
+                          TextFieldWithTitle(
+                            labelText: '이름',
+                            maxLength: 5,
+                            hintText: '이름을 입력해주세요',
+                          ),
+                          SizedBox(height: 40),
+                          TextFieldWithTitle(
+                            labelText: '품종',
+                            maxLength: 8,
+                            hintText: '품종을 입력해주세요',
+                          ),
+                          SizedBox(height: 40),
+                          TextFieldWithTitle(
+                            labelText: '생년월일',
+                            hintText: '2000-08-07 형식으로 입력해주세요',
+                            keyboardType: TextInputType.datetime,
+                          ),
+                          SizedBox(height: 40),
+                          TextFieldWithTitle(
+                            labelText: '만날 날',
+                            hintText: '2000-08-07 형식으로 입력해주세요',
+                            keyboardType: TextInputType.datetime,
+                          ),
+                          SizedBox(height: 40),
+                          Text(
+                            '성별',
+                            style: TextStyle(
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18,
+                              color: Pallete.black,
+                              letterSpacing: -0.45,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Wrap(
+                            runSpacing: 8.0, // 상하
+                            spacing: 8.0, // 좌우
+                            children: this.genderTypes.map((pet) {
+                              return ChoiceChip(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  side: BorderSide(
+                                    color: this.selectedGender == pet
+                                        ? Pallete.subGreen
+                                        : Pallete.lightGray,
+                                    width: 1.0,
+                                  ),
+                                ),
+                                label: Text(pet),
+                                backgroundColor: Pallete.white,
+                                selected: this.selectedGender == pet,
+                                selectedColor: Pallete.mainGreen,
+                                showCheckmark: false,
+                                labelStyle: TextStyle(
+                                  fontFamily: 'Pretendard',
+                                  fontWeight: this.selectedGender == pet
+                                      ? FontWeight.w600
+                                      : FontWeight.w400,
+                                  fontSize: 16,
+                                  color: this.selectedGender == pet
+                                      ? Pallete.white
+                                      : Pallete.lightGray,
+                                ),
+                                onSelected: (bool isSelected) {
+                                  setState(() {
+                                    this.selectedGender =
+                                        isSelected ? pet : null;
+                                  });
+                                },
+                              );
+                            }).toList(),
+                          ),
+                          SizedBox(height: 30),
+                          Text(
+                            '중성화',
+                            style: TextStyle(
+                              fontFamily: 'Pretendard',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18,
+                              color: Pallete.black,
+                              letterSpacing: -0.45,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Wrap(
+                            runSpacing: 8.0, // 상하
+                            spacing: 8.0, // 좌우
+                            children: this.NeuteringTypes.map((pet) {
+                              return ChoiceChip(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  side: BorderSide(
+                                    color: this.selectedNeutering == pet
+                                        ? Pallete.subGreen
+                                        : Pallete.lightGray,
+                                    width: 1.0,
+                                  ),
+                                ),
+                                label: Text(pet),
+                                backgroundColor: Pallete.white,
+                                selected: this.selectedNeutering == pet,
+                                selectedColor: Pallete.mainGreen,
+                                showCheckmark: false,
+                                labelStyle: TextStyle(
+                                  fontFamily: 'Pretendard',
+                                  fontWeight: this.selectedNeutering == pet
+                                      ? FontWeight.w600
+                                      : FontWeight.w400,
+                                  fontSize: 16,
+                                  color: this.selectedNeutering == pet
+                                      ? Pallete.white
+                                      : Pallete.lightGray,
+                                ),
+                                onSelected: (bool isSelected) {
+                                  setState(() {
+                                    this.selectedNeutering =
+                                        isSelected ? pet : null;
+                                  });
+                                },
+                              );
+                            }).toList(),
+                          ),
+                          SizedBox(height: 30),
+                        ],
+                      ),
                     ],
                   ),
                 ),
