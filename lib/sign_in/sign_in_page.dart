b4ae76d9e7cc3926686ad4a2e5b9b4ca31691893
moichi <zzikbu@gmail.com>
2024-09-13 06:37:52 +0900

    feat: 구글 소셜로그인 구현

--- lib/sign_in/sign_in_page.dart
+++ lib/sign_in/sign_in_page.dart
@@ -1,10 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:pet_log/pallete.dart';
 import 'package:pet_log/sign_up/sign_up_nickname_page.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  void signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    await FirebaseAuth.instance.signInWithCredential(credential).then((value) {
+      print(value.user?.email);
+    }).onError((error, StackTrace) {
+      print("error $error");
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +46,44 @@ class SignInPage extends StatelessWidget {
           child: Column(
             children: [
               const Spacer(),
+
+              /// 구글
+              GestureDetector(
+                onTap: () {
+                  signInWithGoogle();
+                },
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: const Color(0xFFFFFFFF),
+                      borderRadius: BorderRadius.circular(6.0),
+                      border: Border.all(color: Color(0xFF747775))),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: SvgPicture.asset(
+                          'assets/icons/ic_login_google.svg',
+                        ),
+                      ),
+                      Expanded(
+                        child: const Text(
+                          'Google로 시작하기',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'Pretendard',
+                            fontWeight: FontWeight.w600,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 38),
+                    ],
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 16),
 
               /// 카카오
               GestureDetector(
@@ -43,7 +110,7 @@ class SignInPage extends StatelessWidget {
                       ),
                       Expanded(
                         child: const Text(
-                          '카카오로 시작하기',
+                          'Kakao로 시작하기',
                           textAlign: TextAlign.center,
                           style: TextStyle(
                             fontFamily: 'Pretendard',
@@ -58,7 +125,7 @@ class SignInPage extends StatelessWidget {
                 ),
               ),
 
-              const SizedBox(height: 12),
+              const SizedBox(height: 16),
 
               /// 애플
               GestureDetector(
