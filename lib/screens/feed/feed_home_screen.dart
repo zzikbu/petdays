b8ca8071ed37108b4aad334fd7db35ee0619aef6
moichi <zzikbu@gmail.com>
2024-11-25 18:10:39 +0900

    fix: 피드 HOT/전체 리스트뷰 스크롤이 동기화 되는 문제 해결

--- lib/screens/feed/feed_home_screen.dart
+++ lib/screens/feed/feed_home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:petdays/models/diary_model.dart';
 import 'package:petdays/screens/diary/diary_detail_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -35,13 +36,103 @@ class _FeedHomeScreenState extends State<FeedHomeScreen>
     });
   }
 
-  /// HOT/전체 토글 버튼 위젯
-  Widget _buildToggleButton({
-    required bool isHot,
-    required VoidCallback onTap,
-    required String text,
-  }) {
-    final isSelected = isHot == _isHotFeed;
+  @override
+  void initState() {
+    super.initState();
+    _currentUserId = context.read<User>().uid;
+    _getFeedList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+
+    final feedState = context.watch<FeedState>();
+    final isLoading = feedState.feedStatus == FeedStatus.fetching;
+
+    return Scaffold(
+      backgroundColor: Palette.background,
+      appBar: AppBar(
+        backgroundColor: Palette.background,
+        scrolledUnderElevation: 0,
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        title: Container(
+          height: 48,
+          width: 170,
+          decoration: BoxDecoration(
+            color: const Color(0xFFF1F3F5),
+            borderRadius: BorderRadius.circular(24),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 5),
+            child: Row(
+              children: [
+                FeedToggleButton(
+                  isHot: true,
+                  isSelected: _isHotFeed,
+                  onTap: () => setState(() => _isHotFeed = true),
+                  text: 'HOT',
+                ),
+                FeedToggleButton(
+                  isHot: false,
+                  isSelected: !_isHotFeed,
+                  onTap: () => setState(() => _isHotFeed = false),
+                  text: '전체',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(color: Palette.subGreen))
+          : Stack(
+              children: [
+                // offstage: true => 위젯을 화면에서 숨김
+                // offstage: false => 위젯을 화면에 표시
+                Offstage(
+                  offstage: !_isHotFeed,
+                  child: FeedListView(
+                    feedList: feedState.hotFeedList,
+                    currentUserId: _currentUserId,
+                    isHotFeed: true,
+                    onRefresh: _getFeedList,
+                  ),
+                ),
+                Offstage(
+                  offstage: _isHotFeed,
+                  child: FeedListView(
+                    feedList: feedState.feedList,
+                    currentUserId: _currentUserId,
+                    isHotFeed: false,
+                    onRefresh: _getFeedList,
+                  ),
+                ),
+              ],
+            ),
+    );
+  }
+}
+
+/// HOT/전체 토글 버튼 위젯
+class FeedToggleButton extends StatelessWidget {
+  final bool isHot;
+  final bool isSelected;
+  final VoidCallback onTap;
+  final String text;
+
+  const FeedToggleButton({
+    super.key,
+    required this.isHot,
+    required this.isSelected,
+    required this.onTap,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
@@ -66,83 +157,47 @@ class _FeedHomeScreenState extends State<FeedHomeScreen>
       ),
     );
   }
+}
 
-  @override
-  void initState() {
-    super.initState();
-    _currentUserId = context.read<User>().uid;
-    _getFeedList();
-  }
+/// 피드 리스트뷰 위젯
+class FeedListView extends StatelessWidget {
+  final List<DiaryModel> feedList;
+  final String currentUserId;
+  final bool isHotFeed;
+  final VoidCallback onRefresh;
+
+  const FeedListView({
+    super.key,
+    required this.feedList,
+    required this.currentUserId,
+    required this.isHotFeed,
+    required this.onRefresh,
+  });
 
   @override
   Widget build(BuildContext context) {
-    super.build(context);
+    return RefreshIndicator(
+      color: Palette.subGreen,
+      backgroundColor: Palette.white,
+      onRefresh: () async {
+        onRefresh();
+      },
+      child: ListView.builder(
+        padding: const EdgeInsets.fromLTRB(24, 0, 24, 24),
+        itemCount: feedList.length,
+        itemBuilder: (context, index) {
+          final diaryModel = feedList[index];
+          final isLike = diaryModel.likes.contains(currentUserId);
 
-    final feedState = context.watch<FeedState>();
-    final currentFeedList =
-        _isHotFeed ? feedState.hotFeedList : feedState.feedList;
-    final isLoading = feedState.feedStatus == FeedStatus.fetching;
-
-    return Scaffold(
-      backgroundColor: Palette.background,
-      appBar: AppBar(
-        backgroundColor: Palette.background,
-        scrolledUnderElevation: 0,
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-        title: Container(
-          height: 48,
-          width: 170,
-          decoration: BoxDecoration(
-            color: const Color(0xFFF1F3F5),
-            borderRadius: BorderRadius.circular(24),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 5),
-            child: Row(
-              children: [
-                _buildToggleButton(
-                  isHot: true,
-                  onTap: () => setState(() => _isHotFeed = true),
-                  text: 'HOT',
-                ),
-                _buildToggleButton(
-                  isHot: false,
-                  onTap: () => setState(() => _isHotFeed = false),
-                  text: '전체',
-                ),
-              ],
-            ),
-          ),
-        ),
+          return DiaryCardWidget(
+            diaryModel: diaryModel,
+            index: index,
+            diaryType: isHotFeed ? DiaryType.hotFeed : DiaryType.allFeed,
+            isLike: isLike,
+            showLock: false,
+          );
+        },
       ),
-      body: isLoading
-          ? const Center(
-              child: CircularProgressIndicator(color: Palette.subGreen))
-          : RefreshIndicator(
-              color: Palette.subGreen,
-              backgroundColor: Palette.white,
-              onRefresh: () async {
-                _getFeedList();
-              },
-              child: ListView.builder(
-                padding: const EdgeInsets.fromLTRB(24, 0, 24, 24),
-                itemCount: currentFeedList.length,
-                itemBuilder: (context, index) {
-                  final diaryModel = currentFeedList[index];
-                  final isLike = diaryModel.likes.contains(_currentUserId);
-
-                  return DiaryCardWidget(
-                    diaryModel: diaryModel,
-                    index: index,
-                    diaryType:
-                        _isHotFeed ? DiaryType.hotFeed : DiaryType.allFeed,
-                    isLike: isLike,
-                    showLock: false,
-                  );
-                },
-              ),
-            ),
     );
   }
 }
